2c83fa1448bd9eea137234a901950ac8c8f94a04
Kévin <kevin.courriels@gmail.com>
2020-06-05 13:59:24 +0200

    ADD logger functions
    Same as console.log() with colors and parameters

---
+++ logger.js
@@ -0,0 +1,51 @@
+function log   (...content)  { this.logType(content, "log")    }
+
+function error (...args)     { this.logType(args, "error")  }
+
+function warn  (...args)     { this.logType(args, "warn")   }
+
+function debug (...args)     { this.logType(args, "debug")  }
+
+function cmd   (...args)     { this.logType(args, "cmd")    }
+
+function ready   (...args)     { this.logType(args, "ready")    }
+
+function logType(content, type = "log") {
+  let cssReset = {
+    "color": __color_hex.FgBlack,
+    "bg_color": __color_hex.BgWhite,
+  }
+  let css = {
+    "color": __color_hex.FgBlack,
+    "bg_color": __color_hex.BgWhite,
+  }
+  console.log(content)
+  content = content.join(' ')
+  const utcDate = new Date().toUTCString()
+  switch (type) {
+    case "log": break // Using default css
+    case "cmd": break // Using default css
+    case "warn": {
+      css.bg_color = __color_hex.BgYellow
+    }
+    break
+    case "error": {
+      css.bg_color = __color_hex.BgRed
+    }
+    break
+    case "debug": {
+      css.color = __color_hex.FgGreen
+    }
+    break
+    case "ready": {
+      css.bg_color = __color_hex.BgGreen
+    }
+    break
+    default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
+  }
+  return console.log(
+    `[${utcDate}] %c[${type}]%c : ${content}`,
+    `color:${css.color}; background-color:${css.bg_color};`,
+    `color:${cssReset.color}; background-color:${cssReset.bg_color};`
+  );
+}
