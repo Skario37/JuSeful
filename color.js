e181ad9792af0f048ef2ce2383c61a271fa244c6
Kévin <kevin.courriels@gmail.com>
2020-06-05 13:59:32 +0200

    ADD Colors

---
+++ color.js
@@ -0,0 +1,48 @@
+const __color_console = {
+  "Reset": "\x1b[0m",
+
+  "Bright": "\x1b[1m",
+  "Dim": "\x1b[2m",
+  "Underscore": "\x1b[4m",
+  "Blink": "\x1b[5m",
+  "Reverse": "\x1b[7m",
+  "Hidden": "\x1b[8m",
+
+  "FgBlack": "\x1b[30m",
+  "FgRed": "\x1b[31m",
+  "FgGreen": "\x1b[32m",
+  "FgYellow": "\x1b[33m",
+  "FgBlue": "\x1b[34m",
+  "FgMagenta": "\x1b[35m",
+  "FgCyan": "\x1b[36m",
+  "FgWhite": "\x1b[37m",
+
+  "BgBlack": "\x1b[40m",
+  "BgRed": "\x1b[41m",
+  "BgGreen": "\x1b[42m",
+  "BgYellow": "\x1b[43m",
+  "BgBlue": "\x1b[44m",
+  "BgMagenta": "\x1b[45m",
+  "BgCyan": "\x1b[46m",
+  "BgWhite": "\x1b[47m",
+}
+
+const __color_hex = {
+  "FgBlack": "#000000",
+  "FgRed": "#ff0000",
+  "FgGreen": "#00ff00",
+  "FgYellow": "#ffff00",
+  "FgBlue": "#0000ff",
+  "FgMagenta": "ff00ff",
+  "FgCyan": "#00ffff",
+  "FgWhite": "#ffffff",
+
+  "BgBlack": "#000000",
+  "BgRed": "#ff0000",
+  "BgGreen": "#00ff00",
+  "BgYellow": "#ffff00",
+  "BgBlue": "#0000ff",
+  "BgMagenta": "#ff00ff",
+  "BgCyan": "#00ffff",
+  "BgWhite": "#ffffff",
+}
